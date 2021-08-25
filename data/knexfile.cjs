a6c776e0fa7fafb357f8aa058d7d9bff2557e7e3
GabrielBPessoa <gabriel.braganti@gmail.com>
2021-08-25 16:38:22 -0300

    corrigido numero da porta no arquivo knex

--- data/knexfile.cjs
+++ data/knexfile.cjs
@@ -3,7 +3,7 @@ module.exports = {
 		client: 'pg',
 		connection: {
 			host: process.env.DB_HOST || 'localhost',
-			port: process.env.DB_PORT || 5433,
+			port: process.env.DB_PORT || 5432,
 			user: process.env.DB_USER || 'postgres',
 			password: process.env.DB_PASSWORD || 'postgres',
 			database: process.env.DATABASE || 'postgres',
