1722708830f1ded9ec99ca237d815a56cde425a0
GabrielBPessoa <gabriel.braganti@gmail.com>
2021-10-01 19:44:44 -0300

    relatorio

---
+++ src/services/relatorioService.js
@@ -0,0 +1,21 @@
+import { dbConnect } from '../../data/db.js'
+
+class Relatorio {
+	async orderProdutosByDate() {
+		try {
+			const itens = await dbConnect('entradaProd').orderBy('dataValidade')
+			console.log(itens)
+			if (!itens) {
+				return res.status(404).send({
+					message: 'No Itens Found',
+				})
+			}
+			return itens
+		} catch (err) {
+			console.log(err.message)
+			throw new Error('Something went wrong in orderProdutosByDate')
+		}
+	}
+}
+
+export { Relatorio }
